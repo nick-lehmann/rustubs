92b5abff378e012bbe8a76ae40398c254cdda8bc
Nick Lehmann <nick@lehmann.sh>
2022-07-26 14:43:18 +0200

    Add rustfmt config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 
 use core::panic::PanicInfo;
 
-/// The linker expects the `_start()` symbol. Hence, we disable name mangling to make sure the compiler does not change the name.
+/// The linker expects the `_start()` symbol. Hence, we disable name mangling to
+/// make sure the compiler does not change the name.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
@@ -11,8 +12,9 @@ pub extern "C" fn _start() -> ! {
 
 /// This function is called whenever a panic occurs.
 ///
-/// The `!` return type marks this function as `diverging`, meaning that it never returns.
-/// `PanicInfo` gives us some insight into where the panic has happened. For now, we can not use this information at all.
+/// The `!` return type marks this function as `diverging`, meaning that it
+/// never returns. `PanicInfo` gives us some insight into where the panic has
+/// happened. For now, we can not use this information at all.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
